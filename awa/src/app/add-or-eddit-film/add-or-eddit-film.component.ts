6e6da648ca77e54cb2d1d7423c676799c5960af7
22Nathan <temp22m@gmail.com>
2022-06-16 12:14:02 +0200

    2 files modified

--- awa/src/app/add-or-eddit-film/add-or-eddit-film.component.ts
+++ awa/src/app/add-or-eddit-film/add-or-eddit-film.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -13,24 +20,30 @@ export class AddOrEdditFilmComponent {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   submitFilm() {
-    const div = this.renderer.createElement('div');
-    let card = this.renderer.createElement('div');
-    card.classList.add('container');
-    card.innerHTML =
-      '' +
-      '<div class="card">' +
-      '<h5 class="card-header">' +
-      this.nfControl.value +
-      '</h5>' +
-      '<div class="card-body">' +
-      '<h5 class="card-title">Description</h5>' +
-      '<p class="card-text">' +
-      this.dfControl.value +
-      '</p>' +
-      '</div>' +
-      '</div>';
+    if (!this.nfControl.value && !this.dfControl.value) {
+      console.log('champs vides');
+    } else {
+      const div = this.renderer.createElement('div');
+      let card = this.renderer.createElement('div');
+      card.classList.add('container');
+      card.innerHTML =
+        '' +
+        '<div class="card my-3">' +
+        '<h5 class="card-header">' +
+        this.nfControl.value +
+        '</h5>' +
+        '<div class="card-body">' +
+        '<h5 class="card-title">Description</h5>' +
+        '<p class="card-text">' +
+        this.dfControl.value +
+        '</p>' +
+        '</div>' +
+        '</div>';
 
-    this.renderer.appendChild(div, card);
-    this.renderer.appendChild(this.el.nativeElement, div);
+      this.renderer.appendChild(div, card);
+      this.renderer.appendChild(this.el.nativeElement, div);
+    }
   }
+
+  @Output() rate = new EventEmitter<number>();
 }
