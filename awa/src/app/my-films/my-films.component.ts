8f3c27c8f33e2ae5df6ce48bec7d86a35fb6aaf3
22Nathan <temp22m@gmail.com>
2022-06-21 12:38:51 +0200

    2 files modified

--- awa/src/app/my-films/my-films.component.ts
+++ awa/src/app/my-films/my-films.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Film } from '../film';
 
 @Component({
   selector: 'app-my-films',
   templateUrl: './my-films.component.html',
-  styleUrls: ['./my-films.component.css']
+  styleUrls: ['./my-films.component.css'],
 })
 export class MyFilmsComponent implements OnInit {
+  @Input() films?: Film;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
