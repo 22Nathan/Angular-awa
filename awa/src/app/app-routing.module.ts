8d70659dbb49f6ffc3a211cc7eb9a1f7d333bbb4
22Nathan <temp22m@gmail.com>
2022-06-23 09:43:44 +0200

    1 file modified

--- awa/src/app/app-routing.module.ts
+++ awa/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { MyFilmsComponent } from './my-films/my-films.component';
 
 const routes: Routes = [
+  // {
+  //   path: '/',
+  //   component: AppComponent,
+  // },
+  // {
+  //   path: '/myFilms',
+  //   component: MyFilmsComponent,
+  // },
   {
-    path: '/',
-    component: MyFilmsComponent,
+    path: '**',
+    redirectTo: '/',
   },
-  {
-    path: '/myFilms',
-    component: MyFilmsComponent,
-  }
 ];
 
 @NgModule({
